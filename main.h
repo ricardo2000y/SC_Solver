7b24c7de939d5ffd9cfb1ac237ffdf500bccc40e
ricardo2000y <77980762+ricardo2000y@users.noreply.github.com>
2021-01-26 03:04:21 +0000

    Add files via upload

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 /*It looks like Microsoft has deprecated lots of calls which use buffers to improve code security. However, the solutions they're providing aren't portable. Anyway, if you aren't interested in using the secure version of their calls (like fopen_s), you need to place a definition of _CRT_SECURE_NO_DEPRECATE before your included header files.*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //teste
 typedef struct {
 	int ** matriz;
@@ -10,10 +11,17 @@ typedef struct {
 	int SL;
 	int SC;
 }puzzle;
+typedef struct{
+	int L;
+	int C;
+	//int value;
+}output;
+
 puzzle matriz_struct;
 void aviso(char *);
 //puzzle ler_ficheiro();
 void printMatriz(puzzle);		
 void verif_zero_alone(puzzle);
 void solve_zero_alone(int,int,puzzle,int);
+void save_result(int, int , int );
 
--- main_func.c
+++ main_func.c
@@ -58,12 +58,14 @@ void verif_zero_alone(puzzle matriz_struct) {
 	int zero_location,j,i,alone_z_counter;
 	int z_counter = 0;
 	int funcao_solve_zero_alone = 1;
-	int tipo_de_varramento = 0;
+	//int tipo_de_varramento = 0; -- não era necesário substitui quando chamas a solve_zero_alone
+	int lock = 0;
+	int total_zero_counter=0;
 	//L,C = i,j
 	//na linha 0, quero avançar de coluna a coluna à procura de 0's, se n encontrar ent incrementa a linha
 	do{	
 		alone_z_counter = 0;
-	 	tipo_de_varramento = 0;
+	 	//tipo_de_varramento = 0;
 		for (i = 0; i < matriz_struct.L; i++, z_counter = 0) {
 			for (j = 0; j < matriz_struct.C;j++) {
 				if (matriz_struct.matriz[i][j] == 0) {
@@ -71,24 +73,28 @@ void verif_zero_alone(puzzle matriz_struct) {
 					zero_location = j;
 				}
 				if (j == (matriz_struct.C - 1) && z_counter == 1){
-				solve_zero_alone(i, zero_location, matriz_struct, tipo_de_varramento);
-				alone_z_counter++;
-				}	
+					solve_zero_alone(i, zero_location, matriz_struct, 0);
+					alone_z_counter++;
+				}
+			 	if (lock = 0)	{
+			 		total_zero_counter += 	z_counter;
+				 	if(i == matriz_struct.L-1) lock = 1;
+				}
 			}
 		}//LEITURA POR LINHA REALIZADA
 		printf("\nLEITURA POR LINHA REALIZADA");
 		printMatriz(matriz_struct);
-		tipo_de_varramento = 1;
+		//tipo_de_varramento = 1;
 		for (j = 0; j < matriz_struct.C; j++, z_counter = 0) {
 			for (i = 0; i < matriz_struct.L;i++) {
 				if (matriz_struct.matriz[i][j] == 0){
-				z_counter++;
-				zero_location = i;
+					z_counter++;
+					zero_location = i;
 				}
 				//se estivermos na ultima posição da coluna, e só houver 1 zero, então chamamos a função q resolve
 				if (i == (matriz_struct.L - 1) && z_counter == 1){
-				solve_zero_alone(zero_location, j, matriz_struct, tipo_de_varramento);
-				alone_z_counter++;
+					solve_zero_alone(zero_location, j, matriz_struct, 1);
+					alone_z_counter++;
 				
 				}
 			}
@@ -111,12 +117,14 @@ void solve_zero_alone(int i, int j, puzzle matriz_struct, int tipo_de_varramento
 	int somaLinha = 0, somaColuna = 0, sol_linha = 0, sol_coluna = 0;
 	//printf("\nSOLVE_ZERO_ALONE CHAMADA");
 	int x;
+	
 	if (tipo_de_varramento == 0 /*leitura por linha*/) {
 		for (x = 0; x < matriz_struct.C; x++) {
 			somaLinha = somaLinha + matriz_struct.matriz[i][x];
 		}
 		sol_linha = matriz_struct.SL - somaLinha;
 		matriz_struct.matriz[i][j] = sol_linha;
+		//save_result(i , j , sol_linha , *output_file);
 	}
 	else if (tipo_de_varramento) {
 		for (x = 0; x < matriz_struct.L; x++) {
@@ -124,7 +132,21 @@ void solve_zero_alone(int i, int j, puzzle matriz_struct, int tipo_de_varramento
 		}
 		sol_coluna = matriz_struct.SC - somaColuna;
 		matriz_struct.matriz[i][j] = sol_coluna;
+		//save_result(i , j , sol_coluna , *output_file);
 	}
-	
-		//printf("\nsoma linha = %d, solved 0 is %d", somaColuna, sol_coluna);
 }
+void save_result(int i, int j,int total_zeros){// efetiamente ainda não az nada
+	output output_file[total_zeros]; // rever e pode ser feito com malloc ppbly
+	
+	static int k=0;
+	output_file[k].C =i;
+	output_file[k].L =j;
+	k++;
+/*	if (k== total_zeros){
+		organiza esta porra e imprime também existe a alternativa de guardar por ordem automáticamente 
+		... ou seja usar a função que corre por o file à procura de zeros para dar as coordenadas dos mesmos
+		possivelmente utíl mas precisa de ser feito com atenção(provavelmente elimina a necessidade da variavel q conta o total)
+		
+	}*/
+}
+
